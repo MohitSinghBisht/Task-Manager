d28478204a9cea8c11fbd35e7ed9cf612fbcff60
MohitSinghBisht <rahiviru@gmail.com>
2025-02-23 00:42:32 +0530

    ui updates home page #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { TaskComponent } from "./task/task.component";
 import { ExportComponent } from "./export/export.component";
+import { SettingsComponent } from "./settings/settings.component";
 
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'task', component: TaskComponent },
   { path: 'export', component: ExportComponent },
+  { path: 'settings', component:  SettingsComponent},
 ];
 
 @NgModule({
